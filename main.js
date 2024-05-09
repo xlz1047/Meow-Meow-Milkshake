2091f71b935dcd38b868acb41bcd785947488bcc
maggie <mjk488@drexel.edu>
2024-05-09 15:06:29 -0400

    updated positive reaction functionality

--- main.js
+++ main.js
@@ -521,7 +521,7 @@ function clearResult() {
 }
 
 
-// Function to display result images and text based on the score
+// Function to display result images and text based on the score- coded by Maggie
 function displayResult(score) {
   var resultImage = document.getElementById("resultImage");
   var resultText = document.getElementById("resultText");
